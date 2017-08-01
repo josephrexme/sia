f4c8f4b6372c149da89931ea3bbc15e36312a08d
Joseph Rex <joerexko@gmail.com>
2017-07-31 23:48:58 -0500

    Give anton ears to listen for messages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv').config();
 const RtmClient = require('@slack/client').RtmClient;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
 const bot_token = process.env.SLACK_BOT_TOKEN || '';
@@ -17,10 +18,15 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
   rtm.sendMessage('Hello! I am a sentient droid', channel);
-
-  // Stream messages from the channel and reply to specific keywords
 });
 
 rtm.on(CLIENT_EVENTS)
 
 rtm.start();
+
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
+  // console.log('messages', message);
+  if(message.text === '!about'){
+    rtm.sendMessage('I am a bot and was made by Joseph Rex', channel);
+  }
+});
