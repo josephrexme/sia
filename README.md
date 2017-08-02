2a32006fcbb94e5346f56606781897deb752df39
Joseph Rex <joerexko@gmail.com>
2017-08-02 05:10:56 -0500

    Add brain for Slack communities

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Sia
 Sia (Sentient Intelligent Autobot) is a bot for slack. Sia uses template based intelligency through [rivescript][1] and was made with [Node-Slack-SDK][2]
 
+### Todos
+- Only require prefix in channels, reply everything in DM
+- Run JavaScript and get results
+
 [1]: https://www.rivescript.com
 [2]: https://github.com/slackapi/node-slack-sdk
--- index.js
+++ index.js
@@ -5,21 +5,16 @@ const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
 const token = process.env.SLACK_BOT_TOKEN || '';
+const defaultChannel = process.env.DEFAULT_CHANNEL || 'general';
 
 const rs = new Rivescript();
 const rtm = new RtmClient(token);
-
-const respond = (command) => (
-  {
-    welcome: 'Welcome to the team. Feel free to check out the channels'
-  }[command]
-);
 
 let channel;
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   for(const c of rtmStartData.channels){
-    if(c.is_member && c.name === 'newbot-test') { channel = c.id }
+    if(c.is_member && c.name === defaultChannel) { channel = c.id }
   }
   console.log(`Logged in as ${rtmStartData.self.name} of ${rtmStartData.team.name}, but not connected to channel yet`);
 });
@@ -36,16 +31,10 @@ rtm.start();
 rs.loadDirectory('./brain', () => {
   rs.sortReplies();
   rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-    console.log(message);
     const commandPrefix = '`';
-    const commands = ['welcome', 'yt', 'wild west wednesday'];
-    const firstword = message.text.slice(1).split(' ')[0];
     const reply = rs.reply('Joseph', message.text.slice(1));
-    console.log(firstword, commands.indexOf(firstword));
-    if(message.text.slice(0, 1) === commandPrefix && commands.indexOf(firstword) === -1){
+    if(message.text.slice(0, 1) === commandPrefix){
       rtm.sendMessage(reply, channel);
-    } else if(commands.indexOf(firstword) !== -1){
-      rtm.sendMessage(respond(firstword), message.channel || channel);
     }
   });
 });
