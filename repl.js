f805244bf233f95e393ae49eb631535b8058d171
Joseph Rex <joerexko@gmail.com>
2017-08-09 10:54:04 -0500

    Wrap repl return in try/catch

--- repl.js
+++ repl.js
@@ -3,6 +3,10 @@ const {VM} = require('vm2');
 module.exports = {
   js: (code) => {
     const vm = new VM();
-    return vm.run(typeof code);
+    try {
+      return vm.run(typeof code);
+    } catch (e) {
+      return e;
+    }
   },
 };
