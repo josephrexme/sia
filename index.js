1ad4bb8230ee10901e9bf072c1213bc92a1b0249
Joseph Rex <joerexko@gmail.com>
2017-09-08 12:24:45 -0500

    Remove mathjs

--- index.js
+++ index.js
@@ -1,64 +1,59 @@
-const dotenv = require('dotenv').config();
-const Rivescript = require('rivescript');
-const RtmClient = require('@slack/client').RtmClient;
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const dotenv = require('dotenv').config()
+const Rivescript = require('rivescript')
+const RtmClient = require('@slack/client').RtmClient
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS
 const repl = require('./lib/repl')
-const math = require('./lib/math')
 
-const token = process.env.SLACK_BOT_TOKEN || '';
-const defaultChannel = process.env.DEFAULT_CHANNEL || 'general';
+const token = process.env.SLACK_BOT_TOKEN || ''
+const defaultChannel = process.env.DEFAULT_CHANNEL || 'general'
 
-const rs = new Rivescript();
-const rtm = new RtmClient(token);
+const rs = new Rivescript()
+const rtm = new RtmClient(token)
 
-let channel;
+let channel
 
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   for(const c of rtmStartData.channels){
     if(c.is_member && c.name === defaultChannel) { channel = c.id }
   }
-  console.log(`Logged in as ${rtmStartData.self.name} of ${rtmStartData.team.name}, but not connected to channel yet`);
+  console.log(`Logged in as ${rtmStartData.self.name} of ${rtmStartData.team.name}, but not connected to channel yet`)
 });
 
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-  console.log('Connection opened');
-  // rtm.sendMessage('Hello! I am a sentient droid', channel);
+  console.log('Connection opened')
 });
 
 rtm.on(CLIENT_EVENTS)
 
-rtm.start();
+rtm.start()
 
 rs.loadDirectory('./brain', () => {
-  rs.sortReplies();
+  rs.sortReplies()
   rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-    console.log('Message', message);
-    const commandPrefix = '`';
+    console.log('Message', message)
+    const commandPrefix = '`'
     if(message.text && message.text.length){
-      const nameMatch = /(^|(\s|@))(sia)/i.test(message.text);
+      const nameMatch = /(^|(\s|@))(sia)/i.test(message.text)
       if(message.text.slice(0, 1) === commandPrefix){
         // Take out the command prefix
-        const filteredText = message.text.slice(1);
+        const filteredText = message.text.slice(1)
         // Take the code ignoring first word as command
-        const code = filteredText.slice(filteredText.indexOf(' ') + 1);
+        const code = filteredText.slice(filteredText.indexOf(' ') + 1)
         // Assume first word is language being passed to repl
-        const lang = filteredText.split(' ')[0];
+        const lang = filteredText.split(' ')[0]
+        // If language exists among available repl languages process repl
         if(Object.keys(repl).indexOf(lang) === -1){
-          if(lang == 'math'){
-            rtm.sendMessage(math(code), message.channel)
-          }else{
-            const reply = rs.reply(message.user, filteredText);
-            rtm.sendMessage(reply, message.channel);
-          }
+          const reply = rs.reply(message.user, filteredText)
+          rtm.sendMessage(reply, message.channel)
         }else{
           console.log('repl', code);
-          rtm.sendMessage(repl.js(code), message.channel);
+          rtm.sendMessage(repl.js(code), message.channel)
         }
       }else if(nameMatch){
-        const reply = rs.reply(message.user, message.text.replace(/sia/i, ''));
-        rtm.sendMessage(reply, message.channel);
+        const reply = rs.reply(message.user, message.text.replace(/sia/i, ''))
+        rtm.sendMessage(reply, message.channel)
       }
     }
-  });
-});
+  })
+})
