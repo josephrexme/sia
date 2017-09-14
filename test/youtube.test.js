af90ec3b6dacd286be500557aa188829ea3f1b18
Joseph Rex <joerexko@gmail.com>
2017-09-14 07:44:38 -0500

    Add more tests for youtube lib

--- test/youtube.test.js
+++ test/youtube.test.js
@@ -3,21 +3,22 @@ const youtube  = require('../lib/youtube')
 
 describe('YouTube', function() {
   this.timeout(5000)
-  it('fetches a video URL', done => {
-    youtube("Fuck this shit I'm out").then(reply => {
-      expect(reply).to.equal('https://www.youtube.com/watch?v=5FjWe31S_0g')
-      done()
-    }).catch(done)
-  })
   it('handles empty results gloriously', done => {
     youtube("mabooloRexcaboolo").then(reply => {
       expect(reply).to.equal("I couldn't find any video on that")
       done()
     }).catch(done)
   })
-  it('ensures valid ids', done => {
-    youtube("michael jackson").then(reply => {
-      expect(reply).to.equal("I couldn't find any video on that")
+  it('fetches a video URL', done => {
+    youtube("Fuck this shit I'm out").then(reply => {
+      expect(reply).to.equal('https://www.youtube.com/watch?v=5FjWe31S_0g')
+      done()
+    }).catch(done)
+  })
+  it('falls back for searches that only match channels', done => {
+    const searchPhrase = "PhiberOptics"
+    youtube(searchPhrase).then(reply => {
+      expect(reply.indexOf(`All ${searchPhrase}'s video`)).to.equal(0)
       done()
     }).catch(done)
   })
