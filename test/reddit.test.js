26526d9c733ce50514350cbcd70594bb457e0f4d
Joseph Rex <joerexko@gmail.com>
2017-09-16 02:45:05 -0500

    Puts all entries for ruby repl

---
+++ test/reddit.test.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect
+// const reddit = require('../lib/reddit')
+
+describe('Reddit', function() {
+  it('fetches data', () => {
+    // reddit('paul ryan').then(reply => {
+    //   console.log(reply)
+    //   expect(reply).to.not.equal('something')
+    //   done()
+    // }).catch(done)
+  })
+})
--- test/repl.test.js
+++ test/repl.test.js
@@ -6,29 +6,35 @@ describe('REPL', function() {
     expect(repl.js('Math.ceil(23.4)')).to.equal('24')
   })
   it('can execute Ruby', done => {
-    repl.rb('puts "hello"').then(reply => {
-      expect(reply).to.equal("hello\n")
+    repl.rb('Math::cos(0)').then(reply => {
+      expect(reply).to.equal("1.0\n")
       done()
     }).catch(done)
   })
   it('converts single quotes to double for ruby', done => {
-    repl.rb("puts 'hello'").then(reply => {
+    repl.rb("'hello'").then(reply => {
       expect(reply).to.equal("hello\n")
       done()
     }).catch(done)
   })
+  it('executes nested puts without failure', done => {
+    repl.rb("puts 'hello'").then(reply => {
+      expect(reply).to.equal("hello\n\n")
+      done()
+    }).catch(done)
+  })
   it('prevents system calls and shell execution in ruby', done => {
-    repl.rb("puts `uname`").then(reply => {
+    repl.rb("`uname`").then(reply => {
       expect(reply).to.equal("Unsafe characters found")
       done()
     }).catch(done)
-    repl.rb("puts system(uname)").then(reply => {
+    repl.rb("system(uname)").then(reply => {
       expect(reply).to.equal("Unsafe characters found")
     })
-    repl.rb("puts exec(uname)").then(reply => {
+    repl.rb("exec(uname)").then(reply => {
       expect(reply).to.equal("Unsafe characters found")
     })
-    repl.rb("puts %x(uname)").then(reply => {
+    repl.rb("%x(uname)").then(reply => {
       expect(reply).to.equal("Unsafe characters found")
     })
     repl.rb('require "open3"').then(reply => {
