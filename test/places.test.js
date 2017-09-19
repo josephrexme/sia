d7fa8be3d733847712fa5daf91487b9ee9064f2d
Joseph Rex <joerexko@gmail.com>
2017-09-19 18:15:43 -0500

    Improve test for places and add after success coverage check to travis config

--- test/places.test.js
+++ test/places.test.js
@@ -9,4 +9,20 @@ describe('Places', function() {
       done()
     }).catch(done)
   })
+  it('contains relevant information to find place', done => {
+    places('University of Wisconin, Milwuakee').then(reply => {
+      expect(reply.indexOf('name')).to.not.equal(-1)
+      expect(reply.indexOf('international_phone_number')).to.not.equal(-1)
+      expect(reply.indexOf('formatted_address')).to.not.equal(-1)
+      expect(reply.indexOf('vicinity')).to.not.equal(-1)
+      expect(reply.indexOf('website')).to.not.equal(-1)
+      done()
+    }).catch(done)
+  })
+  it('handles unknown locations', done => {
+    places('Poopland').then(reply => {
+      expect(reply).to.equal("I couldn't get anything on that location")
+      done()
+    }).catch(done)
+  })
 })
