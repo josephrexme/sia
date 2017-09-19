1ace879b336fcdf1721fd70fb730cb27f26ff643
Joseph Rex <joerexko@gmail.com>
2017-09-19 13:31:10 -0500

    Add more test cases for caniuse and install nyc for coverage

--- test/caniuse.test.js
+++ test/caniuse.test.js
@@ -2,9 +2,21 @@ const expect = require('chai').expect
 const caniuse = require('../lib/caniuse')
 
 describe('Can I Use', function() {
-  it('can check for features', done => {
+  it('list support for single matched feature', done => {
     caniuse('css-filters').then(reply => {
       expect(reply).to.be.a('string')
+      done()
+    }).catch(done)
+  })
+  it('returns a list of matched features for ambiguous input', done => {
+    caniuse('filter').then(reply => {
+      expect(reply.slice(1, 34)).to.equal('These were the matches for filter')
+      done()
+    }).catch(done)
+  })
+  it('has a valid response when no feature is matched', done => {
+    caniuse('catnip').then(reply => {
+      expect(reply).to.equal("There's nothing on catnip in the caniuse DB")
       done()
     }).catch(done)
   })
