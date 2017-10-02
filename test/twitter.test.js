ae7387d8a474755c58536597f0d3afb2c357efb4
Joseph Rex <joerexko@gmail.com>
2017-10-01 23:04:53 -0500

    Add Watson library

--- test/twitter.test.js
+++ test/twitter.test.js
@@ -9,4 +9,22 @@ describe('Twitter', function() {
       done()
     }).catch(done)
   })
+  it('replies when no matches found', done => {
+    twitter('buparcesto estorendo').then(reply => {
+      expect(reply).to.equal("I don't think much about it, actually")
+      done()
+    }).catch(done)
+  })
+  it('shows non-viral tweets with no retweets', done => {
+    twitter('RiceTea').then(reply => {
+      expect(reply).to.be.a('string')
+      done()
+    }).catch(done)
+  })
+  it('strips off the RT from retweets', done => {
+    twitter('Donald Trump').then(reply => {
+      expect(reply).to.not.match(/^RT @\w*:\s*/)
+      done()
+    }).catch(done)
+  })
 })
---
+++ test/watson.test.js
@@ -0,0 +1,7 @@
+// const expect = require('chai').expect
+// const watson = require('../lib/watson')
+
+describe('Watson', function() {
+  it('Can reach the conversation API', () => {
+  })
+})
