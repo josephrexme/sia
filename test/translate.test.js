4903fe9d4cb13cf98a9774622cb41ba528236b44
Joseph Rex <joerexko@gmail.com>
2017-10-08 23:29:26 -0500

    Improve translator to translate to any language

--- test/translate.test.js
+++ test/translate.test.js
@@ -4,8 +4,22 @@ const translate = require('../lib/translate')
 describe('Translate', function() {
   this.timeout(5000)
   it('can translate any language to english', done => {
-    translate('嗨').then(reply => {
+    translate('嗨 to english').then(reply => {
       expect(reply).to.equal('Hi')
+      done()
+    }).catch(done)
+  })
+
+  it('can translate to supported languages', done => {
+    translate('good morning to german').then(reply => {
+      expect(reply).to.equal('guten Morgen')
+      done()
+    }).catch(done)
+  })
+
+  it('gives clear explanation for unknown languages', done => {
+    translate('Hey there to vulcan').then(reply => {
+      expect(reply).to.match(/I only understand .+/)
       done()
     }).catch(done)
   })
